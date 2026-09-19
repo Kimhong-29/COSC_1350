03270fc54fb4348be49cdf64e3d5b8a8c772d996
Kimhong-29 <hongglory168@gmail.com>
2026-09-19 02:51:11 +0000

    Ready for grading

---
+++ helloworld.js
@@ -0,0 +1,31 @@
+/*
+Name: KimHong Poun
+File: helloWorld.js
+Date: September 18, 2026
+*/
+
+// Part 1
+let message = "Hello World";
+console.log(message);
+
+// Part 2
+let greeting = "Hi, the program is running successfully!";
+let decimalNumber = 4.5;
+let zeroNumber = 0;
+let negativeNumber = -10;
+let isTrue = true;
+
+console.log(typeof greeting);
+console.log(typeof decimalNumber);
+console.log(typeof zeroNumber);
+console.log(typeof negativeNumber);
+console.log(typeof isTrue);
+
+console.log("Hi!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
